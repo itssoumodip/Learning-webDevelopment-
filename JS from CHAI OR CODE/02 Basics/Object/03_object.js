3fb1493338b2087cbb1563acd7af5faa1e6be7ed
Soumodip Das <soumodipddas2005@gmail.com>
2024-08-25 18:42:43 +0000

    learning..

--- JS from CHAI OR CODE/02 Basics/Object/03_object.js
+++ JS from CHAI OR CODE/02 Basics/Object/03_object.js
@@ -1,40 +1,17 @@
-//Singleton <-Constractor (Literals)
+//Destratring - 
 
-//Object Literals
-// Object.create
-
-const mySum = Symbol("key1")
-
-const JsUser = {
-    name: "Soumodip",
-    "full name": "Soumodip Das",
-    [mySum]: "mykey1",
-    age: 19,
-    location: "kolkata",
-    email: "soumodip@gmail.com",
-    isLoggedIn: false,
-    lastLoginDays: ["Monday", "Saturday"]
+const course = {
+    coursename: "js in hindi",
+    price: "0",
+    courseInstructor: "Soumodip"
 }
+// course.courseInstructor
 
-//printing Method - 
-console.log(JsUser.email)
-console.log(JsUser["email"])
-console.log(JsUser["full name"])
-console.log(JsUser[mySum])
+const {courseInstructor: instructor} = course
+console.log(instructor);
 
-// chenging - 
-JsUser.email = "OS.email.com"
-console.log(JsUser)
-// Object.freeze(JsUser) // it can be change because of Frezze [symbal key]
-JsUser.email = "chatgpt"
-console.log(JsUser)
-
-JsUser.greeting = function(){
-    console.log("Hello Js User")
-}
-
-JsUser.greetingTwo = function(){
-    console.log(`Hello Js User, ${this.name}`)
-}
-console.log(JsUser.greeting())
-console.log(JsUser.greetingTwo())
+// {
+//     "name": "SOUMO",
+//     "coursname": "js in Hindi",
+//     "price": "free"
+// }
