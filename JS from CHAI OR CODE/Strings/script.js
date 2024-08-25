71bff7a055f703d487fc6a3d816e78e1007ead83
Soumodip Das <soumodipddas2005@gmail.com>
2024-08-25 13:49:35 +0000

    leaarning.....

---
+++ JS from CHAI OR CODE/Strings/script.js
@@ -0,0 +1,20 @@
+const name = 'soumodip'
+const repoCount = 50
+
+console.log(`Hello my name is ${name} my repo acc is ${repoCount}`)
+
+const gameName = new String('SOUMODIP')
+console.log(gameName)
+console.log(gameName.length)
+console.log(gameName.toUpperCase())
+console.log(gameName.charAt(2))
+// console.log(gameName.indexOf('s'))
+const New = gameName.substring(0,4) //NO NEGATIVE TAKE FROM 0
+console.log(New)
+
+const anotherNew = gameName.slice(-8,4)
+console.log(anotherNew)
+
+const newString = "      Soumo    "
+console.log(newString)
+console.log(newString.trim())
