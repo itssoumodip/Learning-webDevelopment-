05b22183941ba0f22aff81786d25092f417a35e1
Soumodip Das <soumodipddas2005@gmail.com>
2024-08-25 18:30:08 +0000

    learning....

---
+++ JS from CHAI OR CODE/02 Basics/Arrays/05_object.js
@@ -0,0 +1,17 @@
+//Destratring - 
+
+const course = {
+    coursename: "js in hindi",
+    price: "0",
+    courseInstructor: "Soumodip"
+}
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+console.log(instructor);
+
+// {
+//     "name": "SOUMO",
+//     "coursname": "js in Hindi",
+//     "price": "free"
+// }
