5d1acf8a0eca4be779044e68f97455fb7847f652
Soumodip Das <soumodipddas2005@gmail.com>
2024-08-26 14:19:29 +0000

    Learning...

--- JS from CHAI OR CODE/05 Iterations/08_reduce.js
+++ JS from CHAI OR CODE/05 Iterations/08_reduce.js
@@ -1,1 +1,30 @@
-const
+
+// Acumelator - blank empty variable 
+// Current Value - the array value that is i
+
+const myNums = [1,5,10]
+
+// const myTotal = myNums.reduce(function (acc, cv) {
+//     console.log(`acc: ${acc} and currval: ${cv}`)
+//     return acc+cv
+// }, 0)
+
+const myTotal = myNums.reduce ( (acc,cv)=> acc+cv, 0)
+console.log(myTotal)
+
+const shoppingCart = [
+    {
+        itemName: "Alu",
+        price: "3"
+    },
+    {
+        itemName: "Piyaj",
+        price: "9"
+    },
+    {
+        itemName: "Carrot",
+        price: "10"
+    },
+]
+
+shoppingCart.reduce( () => (),)
