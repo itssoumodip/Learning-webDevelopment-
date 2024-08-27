4be091d2f1705dd9b14fad0a3dc8561a45df059f
Soumodip Das <soumodipddas2005@gmail.com>
2024-08-27 16:28:16 +0000

    project ....

--- JS from CHAI OR CODE/07 Project/02/script.js
+++ JS from CHAI OR CODE/07 Project/02/script.js
@@ -9,7 +9,28 @@ form.addEventListener('submit', function(e) {
     const weight = parseInt(document.querySelector('#weight').value)
     const results = document.querySelector('#results')
 
-    if(height === '' || ) {
-        results.innerHTML = "Please give a valid Height"
+    if(height === '' || height<0 || isNaN(height)) {
+        results.innerHTML = `Please give a valid Height ${Height}`;
+    } else if (weight === '' || weight<0 || isNaN(weight)) 
+    {
+        results.innerHTML = `Please give a valid Weight ${Weight}`;
+    } else 
+    {
+        const bmi = (weight/((height*height)/10000)).toFixed(2)
+        // show the result 
+        document.querySelector('#results').style.color = "red"
+        results.innerHTML = `<span>${bmi}</span>`
     }
+    // if()) {
+    //     results.innerHTML = `Please give a valid Height ${Height}`;
+    // } else if ()) 
+    // {
+    //     results.innerHTML = `Please give a valid Weight ${Weight}`;
+    // } else 
+    // {
+    //     // show the result 
+    //     weight-guide.innerHTML = `<span>${bmi}</span>`
+    // }
+    // // results.innerHTML = `${height}`
+
 })
