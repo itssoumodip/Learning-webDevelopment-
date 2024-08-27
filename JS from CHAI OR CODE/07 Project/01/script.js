39a724924382adfd5fb6b03639bc655cc127a486
Soumodip Das <soumodipddas2005@gmail.com>
2024-08-27 15:17:24 +0000

    Project....

--- JS from CHAI OR CODE/07 Project/01/script.js
+++ JS from CHAI OR CODE/07 Project/01/script.js
@@ -8,7 +8,16 @@ buttons.forEach(function(button) {
         console.log(event)
         console.log(event.target)
         if(event.target.id === 'grey') {
-            body.style.borderColor
+            body.style.backgroundColor = event.target.id;
+        }
+        if(event.target.id === 'white') {
+            body.style.backgroundColor = event.target.id;
+        }
+        if(event.target.id === 'blue') {
+            body.style.backgroundColor = event.target.id;
+        }
+        if(event.target.id === 'yellow') {
+            body.style.backgroundColor = event.target.id;
         }
     })
 
