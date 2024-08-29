b043830b75fe94c67d26d56e53fcca3e69f3e44d
Soumodip Das <soumodipddas2005@gmail.com>
2024-08-29 11:04:49 +0000

    promise learning....

--- JS from CHAI OR CODE/09 Advance One/prmises.js
+++ JS from CHAI OR CODE/09 Advance One/prmises.js
@@ -74,14 +74,39 @@ const promiseFive = new Promise (function (resolve, reject) {
 
 async function consumePromiseFive() {
     try {
-        const response = await PromiseFive    
-        console.log(response)
+        const response = await promiseFive 
+        console.log(response);
     } catch (error) {
-        
+        console.log(error)
     }
 }
 
 consumePromiseFive()
+
+
+
+// async function getAllUsers() {
+//     try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//         // console.log(response)
+//         const data = await response.json() //to convert into json 
+//         console.log(data)
+//     } catch (error) {
+//         console.log("E: ", error)
+//     }
+// }
+// getAllUsers()
+
+fetch('https://jsonplaceholder.typicode.com/users').then((response)=> {
+    return response.json
+})
+.then((data)=> {
+    console.log(data)
+})
+.catch((error)=> console.log(error))
+
+
+
 
 
 
