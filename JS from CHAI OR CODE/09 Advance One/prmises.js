ae7b6b8b622d9cc220c61dabb04b62c533800f93
Soumodip Das <soumodipddas2005@gmail.com>
2024-08-29 10:21:59 +0000

    promise learning....

--- JS from CHAI OR CODE/09 Advance One/prmises.js
+++ JS from CHAI OR CODE/09 Advance One/prmises.js
@@ -1,1 +1,93 @@
-const promiseOne = new Promise()
+// create the promise 
+const promiseOne = new Promise(function (resolve, reject) {
+    //Do an async task 
+    //DB calls, cryptography, netword
+    setTimeout(function () {
+        console.log('Async task is complete');
+        resolve()
+    }, 1000)
+} )
+
+
+// consume the promise 
+promiseOne.then(function () {
+    console.log("Promise Consumed")
+})
+
+
+new Promise(function(resolve, reject) {
+    setTimeout(function () {
+        console.log("Async task 2")
+        resolve()
+    }, 1000)
+}).then(function () {
+    console.log("Async 2 Resolve");
+})
+
+
+
+const promiseThree = new Promise(function(resolve, reject) {
+    setTimeout(function() {
+        resolve({username: "Soumodip", email: "soumodip@das.com"})
+    }, 1000)
+})
+
+
+promiseThree.then (function(user){
+    console.log(user)
+})
+
+
+const promiseFour = new Promise(function(resolve, reject) {
+    setTimeout(function () {
+        let error = true
+        // let error = false
+        if (!error) {
+            resolve({username: "soumodip", password: "123"})
+        } else {
+            reject('ERROR: Something went Wrong !!')
+        }
+    }, 1000)
+})
+
+// promiseFour.then().catch()
+const username = promiseFour.then( (user) => {
+    console.log(user)
+    return user.username
+}).then ((username) => { //it is called channing
+    console.log(username)
+}).catch(function(error) {
+    console.log(error)
+}).finally(() => console.log("The Promise is either resolved or rejected "))  //default - allways execute
+
+const promiseFive = new Promise (function (resolve, reject) {
+    setTimeout(function () {
+        let error = true
+        // let error = false
+        if (!error) {
+            resolve({username: "JavaScript", password: "123"})
+        } else {
+            reject('ERROR: JS went Wrong !!')
+        }
+    }, 1000) 
+})
+
+async function consumePromiseFive() {
+    try {
+        const response = await PromiseFive    
+        console.log(response)
+    } catch (error) {
+        
+    }
+}
+
+consumePromiseFive()
+
+
+
+/*
+NOTS :- then have the relation with resolve 
+we get call back in then means 
+to conect resolve and then -> resolve
+catch for Error
+*/
