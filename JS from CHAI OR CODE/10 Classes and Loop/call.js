3cfff0e64f75b24ba9434743c556c3076548733d
Soumodip Das <soumodipddas2005@gmail.com>
2024-08-30 07:46:50 +0000

    learning...

---
+++ JS from CHAI OR CODE/10 Classes and Loop/call.js
@@ -0,0 +1,15 @@
+function SetUsername (username) {
+    //complex DB calls
+    this.username = username
+    console.log("called")
+}
+
+function creatUser(username, email, password) {
+    SetUsername.call(username)
+
+    this.email = email
+    this.password = password
+} 
+
+const chai = new creatUser ("soumodip", "chai@fb.com", "123")
+console.log(chai)
