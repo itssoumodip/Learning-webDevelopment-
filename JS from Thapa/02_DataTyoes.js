5e4a8e046c7cf285e8f93e85e7163b80fa1a7bad
Soumodip Das <soumodipddas2005@gmail.com>
2025-01-12 12:32:17 +0000

    about datatypes

---
+++ JS from Thapa/02_DataTyoes.js
@@ -0,0 +1,13 @@
+// convert String to Int.
+var n = "10";
+console.log(typeof + n); //its a method
+console.log(typeof Number(n)); //its another method
+
+// convert Int to String
+// let n = 10;
+// console.log(typeof (n + "")); //its a method
+// console.log(typeof String(n)); //its another method
+
+const n1 = "42";
+const n2 = parseInt(n1);
+console.log(n2);
