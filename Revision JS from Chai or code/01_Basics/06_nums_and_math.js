f8d8a6ac2cd3f38e3b3961006e5bb0ffc1dc6572
Soumodip Das <soumodipddas2005@gmail.com>
2025-01-19 13:27:23 +0000

    about number and maths

---
+++ Revision JS from Chai or code/01_Basics/06_nums_and_math.js
@@ -0,0 +1,13 @@
+const score = 400;
+console.log(score);
+
+const balance = new Number (100); //object of NO. 
+console.log(balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed(2)); //for decimal value
+
+const otherNo = 12.7234242;
+console.log(otherNo.toPrecision(2));
+
+const hundreds = 10000000;
+console.log(hundreds.toLocaleString('en-IN'));
