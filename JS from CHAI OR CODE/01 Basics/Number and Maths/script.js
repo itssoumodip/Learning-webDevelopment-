8eef6f30636a891f22b2fadcbe008ab7e315b48d
Soumodip Das <soumodipddas2005@gmail.com>
2025-01-19 13:27:48 +0000

    math nots

--- JS from CHAI OR CODE/01 Basics/Number and Maths/script.js
+++ JS from CHAI OR CODE/01 Basics/Number and Maths/script.js
@@ -18,13 +18,13 @@ console.log(no.toLocaleString('en-IN'))
 
 // MATH ----------------------------
 
-// console.log(Math)
+console.log(Math)
 console.log(Math.abs(-3))
 console.log(Math.round (4.6))
-console.log(Math.ceil (4.6))
-console.log(Math.floor (4.6))
+console.log(Math.ceil (4.2))  //celing value
+console.log(Math.floor (4.6)) //floar value 
 console.log(Math.min (4,6,0,1,-1))
-console.log(Math.max (4,6,0,1,-1))
+console.log(Math.max (4,6,0,1,-1)) 
 
 // Random 
 console.log(Math.random())
