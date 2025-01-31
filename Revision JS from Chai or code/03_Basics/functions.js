58ea111901d4e9aaaa1a6d1980d1ed69d97345ac
Soumodip Das <soumodipddas2005@gmail.com>
2025-01-31 15:52:35 +0000

    about funtions

---
+++ Revision JS from Chai or code/03_Basics/functions.js
@@ -0,0 +1,14 @@
+function sayMyName ()
+{
+    console.log("Soumodip");
+}
+
+sayMyName();
+
+function addTwoNo (number1, number2)
+{
+    let result = number1+number2;
+    return result;
+}
+const result = addTwoNo(3,4);
+console.log("Result : ", result);
