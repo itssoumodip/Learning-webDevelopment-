53a6fa00f767cb0aefde86a7685d3b259a6a3d0c
Soumodip Das <soumodipddas2005@gmail.com>
2025-02-03 16:13:24 +0000

    about for each loop

---
+++ Revision JS from Chai or code/05_Iterations/five.js
@@ -0,0 +1,35 @@
+// for each
+const coding = ['js', 'java', 'python', 'cpp'];
+
+// coding.forEach(function (val) {
+//     console.log(val)
+// })
+
+// coding.forEach( (val) => {
+//     console.log(val)
+// })
+
+// function printMe (item) {
+//     console.log(item)
+// }
+
+// coding.forEach(printMe)
+
+// coding.forEach( (item, index, arr) => {
+//     console.log(item, index, arr)
+// } )
+
+const myCoding = [
+    {
+        name: "soumodip", 
+        title: "Das"
+    },
+    {
+        name: "santonu", 
+        title: "Debnath"
+    }
+]
+
+myCoding.forEach( (item) => {
+    console.log(item.name);
+})
