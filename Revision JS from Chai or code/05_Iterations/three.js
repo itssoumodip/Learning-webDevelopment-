bef3eb4fd6019e4cbe5ceffcd996e7bfb70b338c
Soumodip Das <soumodipddas2005@gmail.com>
2025-02-03 16:00:04 +0000

    about for of loop

---
+++ Revision JS from Chai or code/05_Iterations/three.js
@@ -0,0 +1,38 @@
+// for of
+
+["", "", ""]
+[{},{},{}]
+
+const arr = [1, 2, 3, 4, 5]
+
+for (const num of arr) {
+    console.log(num)
+}
+
+const greetings = "Hello world"
+for (const greet of greetings) {
+    console.log(greet)
+}
+
+// maps
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+console.log(map)
+
+for (const [key, value] of map) {
+    console.log(key, value)
+}
+
+const myObj = {
+    'game1' : 'NFS',
+    'game2' : 'SpiderMan'
+}
+
+// OBJECT CAN NOT BE INTERABLE
+// for (const [key, value] of myObj) {
+    
+// }
