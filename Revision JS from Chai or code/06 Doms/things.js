a30f031c569019600262b4a2c02abe397e7ab2f6
Soumodip Das <soumodipddas2005@gmail.com>
2025-02-04 17:13:58 +0000

    about DOM

---
+++ Revision JS from Chai or code/06 Doms/things.js
@@ -0,0 +1,44 @@
+document.getElementById('title')
+{/* <h1 id=​"title" class=​"heading">​DOM learning Guys​</h1>​ */}
+document.getElementById('title').id
+// 'title'
+document.getElementById('title').class
+// undefined
+document.getElementById('title').className
+// 'heading'
+document.getElementById('title').getAttribute
+// ƒ getAttribute() { [native code] }
+document.getElementById('title').getAttribute('id')
+// 'title'
+document.getElementById('title').getAttribute('class')
+// 'heading'
+document.getElementById('title').setAttribute('class', 'test')
+// undefined
+title
+{/* <h1 id=​"title" class=​"test">​DOM learning Guys​</h1>​ */}
+title.style.backgroundColor = "green"
+// 'green'
+title.style.padding = '15px'
+// '15px'
+
+// Intrestiong things - 
+title.textContent
+// 'DOM learning Guys'
+title.innerHTML
+// 'DOM learning Guys'
+title.innerText
+// 'DOM learning Guys'
+
+// ------------------------------------------
+document.querySelector('ul')
+{/* <ul>​…​</ul>​ */}
+const myul = document.querySelector('ul')
+// undefined
+const tgreen = myul.querySelector('li')
+// undefined
+tgreen.style.backgroundColor = 'green'
+// 'green'
+tgreen.style.padding = '23px'
+// '23px'
+tgreen.innerText = 'five'
+// 'five'
